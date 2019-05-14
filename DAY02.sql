8b6da87011e1498cdd5881f072d7bd6c84c0162f
Hamyeeun <46946714+Hamyeeun@users.noreply.github.com>
2019-05-14 20:23:08 +0900

    02마지막 수업

--- DAY02.sql
+++ DAY02.sql
@@ -153,3 +153,274 @@ EMPNO,  ENAME,  JOB,         HIREDATE
 7902	FORD	ANALYST	    81/12/03
 7934	MILLER	CLERK	    82/01/23
 */
+-- ORDER BY: 정렬
+-- 9) emp 테이블에서 comm을 가장 많이 받는 순서대로(=역순:내림차순 DESC)
+--    empno, ename, job,hiredate, comm칼럼을 조회하시오
+SELECT empno
+      ,ename
+      ,job
+      ,hiredate
+      ,comm
+  FROM emp
+  ORDER BY comm DESC
+  ;
+  -- => null 데이터는 항상 큰 값으로 취급한다.
+  --    따라서 DESC 정렬에서는 위쪽으로 정렬된다.
+  /*
+EMPNO,  ENAME,  JOB,        HIREDATE,   COMM
+-----------------------------------------------
+7369	SMITH	CLERK	    80/12/17	
+7698	BLAKE	MANAGER	    81/05/01	
+7902	FORD	ANALYST	    81/12/03	
+7900	JAMES	CLERK	    81/12/03	
+7839	KING	PRESIDENT	81/11/17	
+7566	JONES	MANAGER	    81/04/02	
+7934	MILLER	CLERK	    82/01/23	
+7782	CLARK	MANAGER	    81/06/09	
+7654	MARTIN	SALESMAN	81/09/28	1400
+7521	WARD	SALESMAN	81/02/22	500
+7499	ALLEN	SALESMAN	81/02/20	300
+7844	TURNER	SALESMAN	81/09/08	0
+  */
+-- 10) emp 테이블에서 
+--    comm은 적은 순서대로, job은 오름차순(영문), ename오름차순(영문)
+--    정렬하여
+--    empno,ename, job,hiredate,comm을 조회하시오
+SELECT empno
+       ,ename
+       ,job
+       ,hiredate
+       ,comm
+  FROM emp
+  ORDER BY comm ASC, job ASC, ename ASC
+  ;
+  /*
+EMPNO,  ENAME,  JOB,        HIREDATE,   COMM
+--------------------------------------------------
+7844	TURNER	SALESMAN	81/09/08	0
+7499	ALLEN	SALESMAN	81/02/20	300
+7521	WARD	SALESMAN	81/02/22	500
+7654	MARTIN	SALESMAN	81/09/28	1400
+7902	FORD	ANALYST	    81/12/03	
+7900	JAMES	CLERK	    81/12/03	
+7934	MILLER	CLERK	    82/01/23	
+7369	SMITH	CLERK	    80/12/17	
+7698	BLAKE	MANAGER	    81/05/01	
+7782	CLARK	MANAGER	    81/06/09	
+7566	JONES	MANAGER	    81/04/02	
+7839	KING	PRESIDENT	81/11/17	
+  */
+-- emp 테이블에서
+-- comm이 적은 순서대로, job은 오름차순, ename은 내림차순으로 정렬하여
+-- empno, ename, job, hiredate, comm을 조회하시오
+SELECT empno
+      ,ename
+      ,job
+      ,hiredate
+      ,comm
+  FROM emp
+  ORDER BY comm, job, ename DESC
+  ;
+/*
+EMPNO,  ENAME,  JOB,        HIREDATE,   COMM
+---------------------------------------------------
+7844	TURNER	SALESMAN	81/09/08	0
+7499	ALLEN	SALESMAN	81/02/20	300
+7521	WARD	SALESMAN	81/02/22	500
+7654	MARTIN	SALESMAN	81/09/28	1400
+7902	FORD	ANALYST	    81/12/03	
+7369	SMITH	CLERK	    80/12/17	
+7934	MILLER	CLERK	    82/01/23	
+7900	JAMES	CLERK	    81/12/03	
+7566	JONES	MANAGER	    81/04/02	
+7782	CLARK	MANAGER	    81/06/09	
+7698	BLAKE	MANAGER	    81/05/01	
+7839	KING	PRESIDENT	81/11/17	
+*/
+--Alias: 별칭
+-- 11)emp 테이블에서
+--    empno => Employee No.
+--    ename => Employee Name
+--    job   => Job Name
+--    세 컬럼의 이름을 별칭으로 바꾸어서 조회
+SELECT empno AS"Employee NO."
+     ,ename "Employee name" --AS 키워드는 생략 가능
+     ,job   "Job name" --AS 키워드는 생략가능
+  FROM emp
+;
+/*
+Employee NO.,   Employee name,  Job name
+----------------------------------------------
+7369	        SMITH	        CLERK
+7499	        ALLEN	        SALESMAN
+7521	        WARD	        SALESMAN
+7566	        JONES	        MANAGER
+7654	        MARTIN	        SALESMAN
+7698	        BLAKE	        MANAGER
+7782	        CLARK	        MANAGER
+7839	        KING	        PRESIDENT
+7844	        TURNER	        SALESMAN
+7900	        JAMES	        CLERK
+7902	        FORD	        ANALYST
+7934	        MILLER      	CLERK
+*/
+
+--12) 한글 별칭을 사용
+--    emp 테이블에서 각 컬럼에 한글 별칭을 사용
+--    empno => 사번
+--    ename => 이 름(한글 별칭 사이에 공백 주의)
+--    job   => 직무
+-- 하여 조회하시오.
+SELECT empno 사번
+       ,ename"이 름" --""는 공백, 특수문자, 대소문자 구분위해..
+       ,job AS 직무
+FROM emp
+;
+/*
+사번,     이 름,  직무
+---------------------------
+7369	SMITH	CLERK
+7499	ALLEN	SALESMAN
+7521	WARD	SALESMAN
+7566	JONES	MANAGER
+7654	MARTIN	SALESMAN
+7698	BLAKE	MANAGER
+7782	CLARK	MANAGER
+7839	KING	PRESIDENT
+7844	TURNER	SALESMAN
+7900	JAMES	CLERK
+7902	FORD	ANALYST
+7934	MILLER	CLERK
+*/
+
+--13) 테이블에 별칭 설정
+--    emp 테이블에서 empno, ename을 조화하시오
+--1. 별칭 없이 구문 작성
+SELECT empno
+      ,ename
+  FROM emp
+;
+/*
+EMPNO,  ENAME
+----------------
+7369	SMITH
+7499	ALLEN
+7521	WARD
+7566	JONES
+7654	MARTIN
+7698	BLAKE
+7782	CLARK
+7839	KING
+7844	TURNER
+7900	JAMES
+7902	FORD
+7934	MILLER
+*/
+--2. SELECT절에 컬럼 나열할 때 앞에 테이블 이름을 붙일 수 있음
+--   테이블 이름을 붙여서 컬럼을 조회
+SELECT emp.empno
+      ,emp.ename
+  FROM emp
+;
+/*
+EMPNO,  ENAME
+------------------
+7369	SMITH
+7499	ALLEN
+7521	WARD
+7566	JONES
+7654	MARTIN
+7698	BLAKE
+7782	CLARK
+7839	KING
+7844	TURNER
+7900	JAMES
+7902	FORD
+7934	MILLER
+*/
+--3. emp 테이블에 e 라는 별칭을 주고
+--   SELECT 절의 컬럼 앞에 테이블 별칭 e를 붙여서 조회
+SELECT e.empno --테이블 별칭이 사용되는 곳
+    ,e.ename
+  FROM emp e --테이블 별칭 사용 위치
+;
+/*
+EMPNO,  ENAME
+----------------
+7369	SMITH
+7499	ALLEN
+7521	WARD
+7566	JONES
+7654	MARTIN
+7698	BLAKE
+7782	CLARK
+7839	KING
+7844	TURNER
+7900	JAMES
+7902	FORD
+7934	MILLER
+*/
+--14) 영문 별칭을 사용 할 때, 쌍따옴표를 사용하지 않으면
+--   => 결과 출력시 별칭이 모두 대문자화 되어 조회됨.
+-- emp 테이블에서
+-- empno => EmployeeNo
+-- ename => "EmployeeName"
+-- job   => "Job NAME"
+SELECT e.empno AS EmployeeNo
+      ,e.ename "EmployeeName"
+      ,e.job "Job Name"
+FROM emp e
+;
+/*
+EMPLOYEENO, EmployeeName,   Job Name
+-----------------------------------------
+7369	    SMITH	        CLERK
+7499	    ALLEN	        SALESMAN
+7521	    WARD	        SALESMAN
+7566	    JONES	        MANAGER
+7654	    MARTIN	        SALESMAN
+7698	    BLAKE	        MANAGER
+7782	    CLARK	        MANAGER
+7839	    KING	        PRESIDENT
+7844	    TURNER	        SALESMAN
+7900	    JAMES	        CLERK
+7902	    FORD	        ANALYST
+7934	    MILLER	        CLERK
+*/
+
+--15) 별칭과 정렬의 조합: 
+--    컬럼에 별칭을 준 경우 ORDER BY 절에 별칭을 사용할 수 있다.
+-- emp 테이블에  e 별칭을 주고 emp 테이블에서
+-- empno => 사번
+-- ename => 이름
+-- job   => 직무
+-- hiredate => 입사일
+-- comm  =>수당
+-- 위와 같이 각 컬럼에 별칭을 주어 조회하고
+-- 수당, 직무, ename를 모두 오름차순으로 정렬
+SELECT e.empno 사번
+    ,e.ename 이름
+    ,e.job 직무
+    ,e.hiredate 입사일
+    ,e.comm 수당
+  FROM emp e
+ORDER BY 수당, 직무, e.ename --정렬을 할 때 별칭을 쓸 수 있고, 컬럼 명을 그대로 써도 됨
+;
+--(10)번과 동일하나 별칭을 사용한 부분만 차이가 남.
+--    =>결과: ODER BY 절에는 별칭과 원래 컬럼명을 섞어 쓸 수 있다.
+/*
+사번,     이름,  직무,        입사일,      수당
+-------------------------------------------------
+7844	TURNER	SALESMAN	81/09/08	0
+7499	ALLEN	SALESMAN	81/02/20	300
+7521	WARD	SALESMAN	81/02/22	500
+7654	MARTIN	SALESMAN	81/09/28	1400
+7902	FORD	ANALYST	    81/12/03	
+7900	JAMES	CLERK	    81/12/03	
+7934	MILLER	CLERK	    82/01/23	
+7369	SMITH	CLERK	    80/12/17	
+7698	BLAKE	MANAGER	    81/05/01	
+7782	CLARK	MANAGER	    81/06/09	
+7566	JONES	MANAGER	    81/04/02	
+7839	KING	PRESIDENT	81/11/17	
+*/
